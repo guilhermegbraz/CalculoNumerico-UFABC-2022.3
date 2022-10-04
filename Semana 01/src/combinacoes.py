05999cee0895ee8fea75c5c74997167be0fb21ef
Gui <guilherme.g.braz@outlook.com>
2022-10-03 23:38:31 -0300

    correções

--- Semana 01/src/combinacoes.py
+++ Semana 01/src/combinacoes.py
@@ -5,7 +5,7 @@ def combinacao3() :
     for d1 in range(1,3):
         for d2 in range (0,3):
             for d3 in range (0, 3):
-                for pot in range (-1, 2, 1):
+                for pot in range (-2, 2, 1):
                     resultado = (d1* pow(3, -1) + d2 * pow(3, -2) + d3 * pow(3, -3)) * pow(3,pot)
                     linha = ("Combinação 0.{}{}{} x 3^{}".format(d1, d2, d3, pot) , resultado);
                     relacao.append(linha)
--- Semana 01/src/exercicio4.py
+++ Semana 01/src/exercicio4.py
@@ -3,46 +3,46 @@ from BiscectionMethod import BiscectionMethod
 
 import math 
 
-### 6.(a)
+### 4.(a)
 def funcao_a(x : float):
     return pow(x,2) - math.sin(x)
 
-### 6.(b)
+### 4.(b)
 def funcao_b(x : float):
     return math.cos(math.pi*(x+1)/8) + 0.148 * x -0.9062
 
-### 6.(c)
+### 4.(c)
 def funcao_c(x : float):
     return pow(x, 2) + math.log(x)
 
-### 6.(d)
+### 4.(d)
 def funcao_d(x : float):
     return x * math.pow(math.e, x) - 1
 
 
 
 def main():
-    print("---------- 6.(a)---------- \n")
+    print("---------- 4.(a)---------- \n")
     problemaA = BiscectionMethod(0.5, 1, 0.01, funcao_a)
     print("Raiz a partir do objeto eh: {}".format(problemaA.get_raiz()))
     print(problemaA.tabela)
     
-    print("\n---------- 6.(b).I---------- \n")
+    print("\n---------- 4.(b).I---------- \n")
     problemaB = BiscectionMethod(-1, 0, 0.01, funcao_b)
     print("Raiz a partir do objeto eh: {}".format(problemaB.get_raiz()))
     print(problemaB.tabela)
     
-    print("\n---------- 6.(b).II---------- \n")
+    print("\n---------- 4.(b).II---------- \n")
     problemaB2 = BiscectionMethod(0, 1, 0.01, funcao_b)
     print("Raiz a partir do objeto eh: {}".format(problemaB2.get_raiz()))
     print(problemaB.tabela)
     
-    print("\n---------- 6.(c)---------- \n")
+    print("\n---------- 4.(c)---------- \n")
     problemaC = BiscectionMethod(0.5, 1, 0.01, funcao_c)
     print("Raiz a partir do objeto eh: {}".format(problemaC.get_raiz()))
     print(problemaC.tabela)
     
-    print("\n---------- 6.(d)---------- \n")
+    print("\n---------- 4.(d)---------- \n")
     problemaD = BiscectionMethod(0.5, 1, 0.01, funcao_d)
     print("Raiz a partir do objeto eh: {}".format(problemaD.get_raiz()))
     print(problemaD.tabela)
